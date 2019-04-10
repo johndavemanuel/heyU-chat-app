e46a23a8f4bf267f3350c381d8714226ac93b366
davemanuel <dave@nextus,asia>
2019-04-10 09:15:53 +0800

    add faces and groups

--- www/js/routes.js
+++ www/js/routes.js
@@ -7,6 +7,14 @@ routes = [
     path: '/home/',
     url: './pages/home.html',
   },
+  {
+    path: '/messages/',
+    url: './pages/messages.html',
+  },
+  {
+    path: '/groups/',
+    url: './pages/groups.html',
+  },
   // Default route (404 page). MUST BE THE LAST
   {
     path: '(.*)',
